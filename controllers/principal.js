b4a3fa7ca12b18103c1ed62a947de1eca67a0bfe
ErikLopez <espinozalopezerik@gmail.com>
2025-02-12 17:37:06 -0600

    Plantilla de menus

--- controllers/principal.js
+++ controllers/principal.js
@@ -1,11 +1,18 @@
-
+import { Menu } from '../models/asociaciones.js';
 const nosotros = (req, res) => {
     res.render('invitado/nosotros');
 }
 
-const menu = (req, res) => {
-    res.render('invitado/menu');
-}
+const menu = async (req, res) => {
+    const bebidas = await Menu.findAll({ where: { categoriaId: 1 } });
+    const desayunos = await Menu.findAll({ where: { categoriaId: 2 } });
+    const sopas = await Menu.findAll({ where: { categoriaId: 3 } });
+    const platoFuerte = await Menu.findAll({ where: { categoriaId: 4 } });
+    const postres = await Menu.findAll({ where: { categoriaId: 5 } });
+
+    res.render('invitado/menu', { bebidas, desayunos, sopas, platoFuerte, postres });
+};
+
 
 const desayunos = (req, res) => {
     res.render('invitado/desayunos');
