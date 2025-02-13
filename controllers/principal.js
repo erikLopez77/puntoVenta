2cef9bc8fd9376f4c9950b80acff0a065910e7d9
ErikLopez <espinozalopezerik@gmail.com>
2025-02-12 21:56:37 -0600

    vistas de menu hechas, falta vista carrito

--- controllers/principal.js
+++ controllers/principal.js
@@ -10,24 +10,34 @@ const menu = async (req, res) => {
     const platoFuerte = await Menu.findAll({ where: { categoriaId: 4 } });
     const postres = await Menu.findAll({ where: { categoriaId: 5 } });
 
-    res.render('invitado/menu', { bebidas, desayunos, sopas, platoFuerte, postres });
+    res.render('invitado/menu', { bebidas, desayunos, sopas, platoFuerte, postres, pagina: 'Menu general' });
 };
 
 
-const desayunos = (req, res) => {
-    res.render('invitado/desayunos');
+const desayunos = async (req, res) => {
+    const desayunos = await Menu.findAll({ where: { categoriaId: 2 } });
+
+    res.render('invitado/desayunos', { desayunos, pagina: 'Desayunos' });
 }
-const sopas = (req, res) => {
-    res.render('invitado/sopas');
+const sopas = async (req, res) => {
+    const sopas = await Menu.findAll({ where: { categoriaId: 3 } });
+
+    res.render('invitado/sopas', { sopas, pagina: 'Sopas y pastas' });
 }
-const platoFuerte = (req, res) => {
-    res.render('invitado/plato-fuerte');
+const platoFuerte = async (req, res) => {
+    const platosFuertes = await Menu.findAll({ where: { categoriaId: 4 } });
+
+    res.render('invitado/plato-fuerte', { platosFuertes, pagina: 'Plato fuerte' });
 }
-const postres = (req, res) => {
-    res.render('invitado/postres');
+const postres = async (req, res) => {
+    const postres = await Menu.findAll({ where: { categoriaId: 5 } });
+
+    res.render('invitado/postres', { postres, pagina: 'Postres' });
 }
-const bebidas = (req, res) => {
-    res.render('invitado/bebidas');
+const bebidas = async (req, res) => {
+    const bebidas = await Menu.findAll({ where: { categoriaId: 1 } });
+
+    res.render('invitado/bebidas', { bebidas, pagina: 'Bebidas' });
 }
 
 export {
